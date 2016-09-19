a14af91926998ab7c7146f30f30b8ad5d70c587f
tizardef <tizardef@g.cofc.edu>
2016-09-19 15:11:38 -0400

    edited student enrollment assignment for day24 v2

--- Day24/StudentEnrollment/StudentEnrollment/src/ssa/Mainline.java
+++ Day24/StudentEnrollment/StudentEnrollment/src/ssa/Mainline.java
@@ -10,18 +10,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public class EnrolledStudent {
+public class Mainline {
 	
 	public static Connection  myConn=null;
 	public static PreparedStatement myStmt=null;
 	public static ResultSet myRs=null;
+	
+	
 
 	public static void main(String[] args) throws FileNotFoundException, IOException, SQLException {
 		// TODO Auto-generated method stub
 		
-//assignMajor();
-//display();
+assignMajor();
+display();
 signUpForClass();
+enrollStudent();
 	}
 	
 	
@@ -37,43 +40,81 @@ signUpForClass();
 		
 	}
 	
-//	private static void enrollStudent() throws SQLException{
-//		
-//		try{
-//			makeConnection();
-//			myStmt= myConn.prepareStatement("Insert into student(first_name, last_name, sat, gpa) values(?,?,?,?)");
-//			
-//			
-//			myStmt.setString(1, "Lou");
-//			myStmt.setString(2, "Zar");
-//			myStmt.setInt(3, 950);
-//			myStmt.setDouble(4, 3.0);
-//			myStmt.executeUpdate();
-//			
-//		}catch(Exception ex){
-//			
-//		}finally{
-//			
-//		}
-//		
-//	}
+	private static void enrollStudent() throws SQLException{
+		
+		try{
+			makeConnection();
+			myStmt= myConn.prepareStatement("Insert into student(first_name, last_name, sat, gpa) values(?,?,?,?)");
+			
+			
+			myStmt.setString(1, "Adam");
+			myStmt.setString(2, "Zapel");
+			myStmt.setInt(3, 1200);
+			myStmt.setDouble(4, 3.0);
+			myStmt.executeUpdate();
+			
+			myStmt.setString(1, "Graham");
+			myStmt.setString(2, "Krakir");
+			myStmt.setInt(3, 500);
+			myStmt.setDouble(4, 2.5);
+			myStmt.executeUpdate();
+			
+			myStmt.setString(1, "Ella");
+			myStmt.setString(2, "Vader");
+			myStmt.setInt(3, 800);
+			myStmt.setDouble(4, 3.0);
+			myStmt.executeUpdate();
+			
+			myStmt.setString(1, "Stanley");
+			myStmt.setString(2, "Kupp");
+			myStmt.setInt(3, 1350);
+			myStmt.setDouble(4, 3.3);
+			myStmt.executeUpdate();
+			
+			myStmt.setString(1, "Lou");
+			myStmt.setString(2, "Zar");
+			myStmt.setInt(3, 950);
+			myStmt.setDouble(4, 3.0);
+			myStmt.executeUpdate();
+			
+			
+		}catch(Exception ex){
+			
+		}finally{
+			
+		}
+		
+	}
 	
 	private static void assignMajor() throws SQLException{
-		
-		//fetchSat();
-	//	fetchMajor();
-		
-		
+				
 	
 		try{
 			makeConnection();
 			myStmt= myConn.prepareStatement("update student set major_id= ? where id=?");
 			
+			myStmt.setInt(1, 3);
+			myStmt.setInt(2, 191);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 7);
+			myStmt.setInt(2, 192);
+			myStmt.executeUpdate();
+
+			myStmt.setInt(1, 2);
+			myStmt.setInt(2, 193);
+			myStmt.executeUpdate();
+
+			myStmt.setInt(1, 5);
+			myStmt.setInt(2, 194);
+			myStmt.executeUpdate();
+
 			myStmt.setInt(1, 6);
 			myStmt.setInt(2, 195);
 			myStmt.executeUpdate();
+
 			
-			//display();
+			
 			
 		}catch(Exception ex){
 			
@@ -88,68 +129,9 @@ signUpForClass();
 		
 	}
 	
-	private static void fetchMajor() throws SQLException{
-
-		try{
-			makeConnection();
-			
-			myStmt= myConn.prepareStatement("select student.first_name, student.last_name, major.description from student join major on student.major_id = major_id");
-			
-			//Set parameter values
-			
-			
-			//Execute the Query
-			myRs= myStmt.executeQuery();
-			while(myRs.next())
-				System.out.println(myRs.getString("first_name") + myRs.getString("last_name")  + myRs.getString("description"));
-		}catch(Exception ex){
-			//ex.printStackTrace();
-		}finally{
-			
-			
-			if(myConn !=null)
-				myConn.close();	
-
-			if(myStmt !=null)
-				myStmt.close();
-			if(myRs !=null)
-				myRs.close();
-			
-				
-			
-		}
-
-		}
 	
-	private static void fetchSat() throws SQLException{
 
-		try{
-			makeConnection();
-			
-			myStmt= myConn.prepareStatement("select sat from student where id = ");
-			
-			//Set parameter values
-			myStmt.setInt(1, 191);
-			//myStmt.
-			
-			//Execute the Query
-			myRs= myStmt.executeQuery();
 		
-			if(myRs.getInt("sat") >= 900){
-				assignMajor();
-			}else System.out.println("SAT score does not meet major requirement");
-				
-		
-			
-			
-		}catch(Exception ex){
-			//ex.printStackTrace();
-		}finally{
-			
-			
-		}
-
-		}
 
 	private static void display() throws SQLException {
 		try {
@@ -160,28 +142,14 @@ signUpForClass();
 					+ "major.description from student join major on student.major_id = major.id) as table1"
 					+ " join major_class_relationship on table1.major_id = major_class_relationship.major_id ) as table2"
 					+ " join class on table2.class_id=class.id;");
-			
-			//input the parameters
-			myStmt.setString(1, "?");
-			myStmt.setString(2, "?");
-			myStmt.setInt(3, x );
-			myStmt.setString(4, "?");
-			myStmt.setInt(5, x);
-			myStmt.setString(6, "?");
-			myStmt.setInt(7, x);
-			myStmt.setString(8, "?");
-			myStmt.setInt(9, x);
-			myStmt.setString(10, "?");
-			myStmt.setInt(11, x);
-			myStmt.setString(12, "?");
-			
+
 			myRs= myStmt.executeQuery();
 			System.out.println("Education System - Enrollment Process");
 			System.out.println("=====================================");
 			System.out.println("Enrolled"+ myRs.getString("first_name") + " " +  myRs.getString("last_name") + " " + "as a new student." + "\n" + myRs.getString("first_name") + " " + myRs.getString("last_name") + " " + "has an SAT score of" + " " + myRs.getInt("sat") + "." + "\n" + "Asigned" + " " + myRs.getString("first_name") + " " + myRs.getString("last_name") + " " + "to the" + " " + myRs.getInt("description") + " " + "major which requires a required SAT score of" + " " + myRs.getInt("sat") + "\n" + "Enrolled" + " " + myRs.getString("first_name") + " " + myRs.getString("last_name") + " " + "in the following classes:" + "\n" + "1." + " " + myRs.getString("subject") + " " + myRs.getInt("section") + " " + "required for major" + "\n" + "2." + " " + myRs.getString("subject") + " " + myRs.getInt("section") + " " + "required for major" + "\n" + "3." + " " + myRs.getString("subject") + " " + myRs.getInt("section") + " " + " not required for major" + "4." + " " + myRs.getString("subject") + " " + myRs.getInt("section") + " " + " not required for major" );
 			
 			
-			//System.out.println(myRs.getString("first_name") + myRs.getString("last_name")  + myRs.getString("description"));
+			
 		}catch(Exception ex){
 			
 		}finally{
@@ -207,11 +175,88 @@ private static void signUpForClass() throws SQLException{
 			makeConnection();
 			myStmt= myConn.prepareStatement("insert into student_class_relationship (student_id, class_id) values (?, ?)");
 			
-			myStmt.setInt(1, 193);
-			myStmt.setInt(2, 40442);
+			myStmt.setInt(1, 191);
+			myStmt.setInt(2, 10101);
 			myStmt.executeUpdate();
 			
-			//display();
+			myStmt.setInt(1, 191);
+			myStmt.setInt(2, 10102);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 191);
+			myStmt.setInt(2, 60222);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 191);
+			myStmt.setInt(2, 40312);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 192);
+			myStmt.setInt(2, 30101);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 192);
+			myStmt.setInt(2, 30202);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 192);
+			myStmt.setInt(2, 10101);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 192);
+			myStmt.setInt(2, 10102);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 193);
+			myStmt.setInt(2, 40311);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 193);
+			myStmt.setInt(2, 40312);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 193);
+			myStmt.setInt(2, 20202);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 193);
+			myStmt.setInt(2, 20203);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 194);
+			myStmt.setInt(2, 40311);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 194);
+			myStmt.setInt(2, 40312);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 194);
+			myStmt.setInt(2, 20202);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 194);
+			myStmt.setInt(2, 20203);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 195);
+			myStmt.setInt(2, 60221);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 195);
+			myStmt.setInt(2, 60222);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 195);
+			myStmt.setInt(2, 10101);
+			myStmt.executeUpdate();
+			
+			myStmt.setInt(1, 195);
+			myStmt.setInt(2, 10102);
+			myStmt.executeUpdate();
+			
+			
+			
 			
 		}catch(Exception ex){
 			
