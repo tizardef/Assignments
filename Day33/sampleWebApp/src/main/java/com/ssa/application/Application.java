0d5ed570c1efe4a98d6f29570587690da8f3cede
tizardef <tizardef@g.cofc.edu>
2016-09-28 16:36:55 -0400

    added WebApp

---
+++ Day33/sampleWebApp/src/main/java/com/ssa/application/Application.java
@@ -0,0 +1,28 @@
+package com.ssa.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages="com.ssa")
+
+@SpringBootApplication(exclude = { HibernateJpaAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class})
+public class Application extends SpringBootServletInitializer  {  
+    
+    public static void main(String[] args) {
+       SpringApplication.run(Application.class, args);
+   } 
+    
+     @Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+       return application.sources(Application.class);
+   }
+
+}
+
+
+
